141b263365cfdac71d3330618216615b2af62e6e
Noah Moore <admin@noahkmoore.com>
2022-06-30 13:45:21 +0300

    working on db

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -19,4 +20,11 @@ class Forms(APIView):
 
     def post(self, request):
         print(request.data)
-        return Response("working", status=200)
+        values = ["First Name", "Middle Name", "Last Name", "Cell Phone", "Email", "Street Address",
+                  "Street Address (line 2)", "City", "State", "Zip/Postal Code", "Country", "DOB", "Country of Birth",
+                  "Country Issuing Passport", "Citizenship"]
+        first_name, middle_name, last_name, cell_phone, email, street_address, street_address_1, city, state, \
+        postal_code, country, DOB, country_of_birth, country_issuing_passport, citizenship = \
+            (request.data.get(val) for val in values)
+        print(first_name, last_name, email, )
+        return HttpResponse(status=200)
