774892a36e23e4934854b981ece1a7aebffabdfc
Noah Moore <wobblybloom92@gmail.com>
2022-07-05 18:33:31 +0300

    first app done

--- api/models.py
+++ api/models.py
@@ -3,11 +3,35 @@ from django.db import models
 
 # Create your models here.
 
-class HealthForm(models.Model):
-    name = models.CharField(max_length=128)
-    file = models.FileField(max_length=5, upload_to="health_forms/")
+# class HealthForm(models.Model):
+#     name = models.CharField(max_length=128)
+#     file = models.FileField(max_length=5, upload_to="health_forms/")
 
 
-class DisclosureAgreement(models.Model):
-    name = models.CharField(max_length=128)
-    file = models.FileField(max_length=5, upload_to="disclosure_agreement/")
+# class DisclosureAgreement(models.Model):
+#     name = models.CharField(max_length=128)
+
+def user_directory_path(instance):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return f'v5/{instance.user.first_name}_{ instance.user.last_name}'
+
+class Profile(models.Model):
+    middle_name = models.CharField(max_length=255)
+    cell_phone = models.CharField(max_length=255)
+    address_1 = models.CharField(max_length=255)
+    address_2 = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    postal_code = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    dob = models.DateField()
+    country_of_birth = models.CharField(max_length=255)
+    country_issuing_passport = models.CharField(max_length=255)
+    citizenship = models.CharField(max_length=255)
+    israeli_citizenship = models.BooleanField()
+    israeli_passport = models.BooleanField()
+    current_school = models.CharField(max_length=255)
+    synagogue = models.CharField(max_length=255)
+    high_school_transcripts = models.FileField(upload_to=f'{user_directory_path}/high_school_transcripts')
+    photo = models.ImageField()
+    resume = models.FileField(upload_to=f'{user_directory_path}/resume')
--- api/views.py
+++ api/views.py
@@ -1,3 +1,6 @@
+from api.models import Profile
+from api.serializers import UserSerializer
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework.response import Response
@@ -20,11 +23,20 @@ class Forms(APIView):
 
     def post(self, request):
         print(request.data)
-        values = ["First Name", "Middle Name", "Last Name", "Cell Phone", "Email", "Street Address",
-                  "Street Address (line 2)", "City", "State", "Zip/Postal Code", "Country", "DOB", "Country of Birth",
-                  "Country Issuing Passport", "Citizenship"]
-        first_name, middle_name, last_name, cell_phone, email, street_address, street_address_1, city, state, \
-        postal_code, country, DOB, country_of_birth, country_issuing_passport, citizenship = \
+        values = ["First Name", "Middle Name", "Last Name", "Cell Phone", "Email", "Street Address", "Street Address (line 2)", "City", "State", "Zip/Postal Code", "Country", "DOB", "Country of Birth",
+                  "Country Issuing Passport", "Citizenship", "Israeli Citizenship", "Israeli Passport", "Current School", "Synogogue", "High School Transcripts", "Photo", "Resume", "Password"]
+        first_name, middle_name, last_name, cell_phone, email, street_address, street_address_1, \
+        city, state, postal_code, country, DOB, country_of_birth, country_issuing_passport, citizenship,\
+        israeli_citizenship, israeli_passport, current_school, synagogue, hs_transcripts, photo, resume, password = \
             (request.data.get(val) for val in values)
-        print(first_name, last_name, email, )
+        user = User.objects.create(first_name=first_name, last_name=last_name, email=email)
+        user.set_password(password)
+        user.save()
+        print(UserSerializer(user).data)
+        profile = Profile.objects.create(middle_name=middle_name, cell_phone=cell_phone, address_1=street_address,
+         address_2=street_address_1, city=city, state=state, postal_code=postal_code, country=country, dob=DOB, 
+         country_of_birth=country_of_birth, country_issuing_passport=country_issuing_passport, citizenship=citizenship, 
+         israeli_citizenship=israeli_citizenship, israeli_passport=israeli_passport, current_school=current_school, 
+         synagogue=synagogue, high_school_transcripts=hs_transcripts, photo=photo, resume=resume)
+        print(str(profile))
         return HttpResponse(status=200)
